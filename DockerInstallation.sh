d7432a79b94405f45d8ef7274a3db4fd8971b80f
Super Manito <68613938+SuperManito@users.noreply.github.com>
2021-07-06 04:40:46 +0800

    Docker Engine & Docker Compose 一键安装脚本

--- DockerInstallation.sh
+++ DockerInstallation.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-wget https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.x -O install.x
-chmod +x install.x
-./install.x
+wget https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.x -O install.x && chmod +x install.x && ./install.x
